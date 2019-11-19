994380ce7ca20420b34009e14a5ae3b65069f5e6
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 11:33:17 +0100

    added method to extract entity code from uri

---
+++ projects/11/biokb/utils.py
@@ -0,0 +1,10 @@
+def uri_to_code(uri: str) -> str:
+    """Translates URIs such as http://lcsb.uni.lu/biokb/entities/BTO_0001043 to BTO_0001043
+    
+    Arguments:
+        uri {str} -- [description]
+    
+    Returns:
+        str -- [description]
+    """
+    return uri.split('/')[-1]
