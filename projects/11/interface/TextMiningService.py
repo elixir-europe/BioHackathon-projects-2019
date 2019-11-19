fd2002fbf451e233dc1ff8c3082e28642cdc2058
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 14:49:45 +0100

    added type filter to get cooccurrrences

--- projects/11/interface/TextMiningService.py
+++ projects/11/interface/TextMiningService.py
@@ -26,7 +26,7 @@ class TextMiningService(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def get_co_occurrences(self, entity: str, limit: int = 20) -> List[CoOccurrence]:
+    def get_co_occurrences(self, entity: str, limit: int = 20, types: List[str] = []s) -> List[CoOccurrence]:
         """
         Co-occurrences at publication level.
 
