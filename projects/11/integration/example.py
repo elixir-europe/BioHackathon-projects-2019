fd7a5b75a4b664a408c0c50ce4f36612f61ddcfb
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 17:37:58 +0100

    fixed imports

--- projects/11/integration/example.py
+++ projects/11/integration/example.py
@@ -1,7 +1,7 @@
 import logging
 
 from biokb.biokb import BioKBService
-from jensenLabService import JensenLabService
+from jensenLabService.jensenLabService import JensenLabService
 
 logger = logging.getLogger(__name__)
 
@@ -9,5 +9,6 @@ if __name__ == '__main__':
     text_mining_services = [JensenLabService(), BioKBService()]
     for text_mining_service in text_mining_services:
         print("Using service {}".format(text_mining_service.name))
-        publications = text_mining_service.get_mentions(["DOID:10652", "DOID:10935"], limit=100)
+        publications = text_mining_service.get_mentions(
+            ["DOID:10652", "DOID:10935"], limit=100)
         print(", ".join([str(p) for p in publications]))
