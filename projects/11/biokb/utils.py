e6d403a598cedec5350018a85e94fea488b9c64e
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 12:19:01 +0100

    added method to convert entity codes from colon to underscore syntax

--- projects/11/biokb/utils.py
+++ projects/11/biokb/utils.py
@@ -1,5 +1,10 @@
-def uri_to_code(uri: str) -> str:
-    """Translates URIs such as http://lcsb.uni.lu/biokb/entities/BTO_0001043 to BTO_0001043
+def standarise_underscored_entity_code(entity_code: str) -> str:
+    return entity_code.replace(':', '_', 1)
+
+
+def uri_to_entity_code(uri: str) -> str:
+    """Translates URIs such as http://lcsb.uni.lu/biokb/entities/BTO_0001043 to BTO:0001043
+    Replaces only the first underscore with colon.
 
     Arguments:
         uri {str} -- [description]
@@ -7,4 +12,4 @@ def uri_to_code(uri: str) -> str:
     Returns:
         str -- [description]
     """
-    return uri.split('/')[-1]
+    return uri.split('/')[-1].replace('_', ':', 1)
