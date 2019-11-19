a055097135a69a506a68460049fdbdc1f3c08351
Valentin Grouès <valentin.groues@uni.lu>
2019-11-19 11:04:16 +0100

    add dummy service implementation

--- projects/11/dummy-service/__init__.py
+++ projects/11/dummy-service/__init__.py
@@ -1,4 +1,2 @@
-import logging
 
-logger = logging.getLogger(__name__)   
  
--- projects/11/dummy-service/dummyService.py
+++ projects/11/dummy-service/dummyService.py
@@ -1,4 +1,17 @@
 import logging
+from typing import List
 
-logger = logging.getLogger(__name__)   
- 
+from interface.TextMiningService import TextMiningService, Publication
+
+logger = logging.getLogger(__name__)
+
+
+class DummyService(TextMiningService):
+    def __init__(self):
+        super().__init__("DummyService", "Service that returns dummy results")
+
+    def get_mentions(self, entities: List, limit: int = 20) -> List[Publication]:
+        return [Publication(pm_id="00000" + str(i)) for i in range(20)]
+
+    def get_co_occurrences(self, entity: str) -> List[str]:
+        pass
