42f4e24f2c3dc0fd3a742612910a012665ac27b0
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 17:43:43 +0100

    fixed imports and renamed folders

--- projects/11/jensenLabService/jensenLabService.py
+++ projects/11/jensenLabService/jensenLabService.py
@@ -4,7 +4,8 @@ from typing import List
 
 import requests
 
-from TextMiningService import TextMiningService, Publication
+from interface.TextMiningService import TextMiningService
+from models.publication import Publication
 
 logger = logging.getLogger(__name__)
 
@@ -30,7 +31,8 @@ class JensenLabService(TextMiningService):
         else:
             limit_per_entity = JensenLabService.LIMIT_PER_ENTITY
         for (entity, entity_type) in entities_and_types:
-            publications_ids.append(self.get_mentions_for_entity(entity, entity_type, limit=limit_per_entity))
+            publications_ids.append(self.get_mentions_for_entity(
+                entity, entity_type, limit=limit_per_entity))
         publications_ids_intersection = set.intersection(*publications_ids)
         return [Publication(pm_id=pid) for pid in publications_ids_intersection][0:limit]
 
@@ -54,7 +56,8 @@ class JensenLabService(TextMiningService):
 
     @staticmethod
     def get_mentions_for_entity(entity, entity_type, limit):
-        url_mentions = JensenLabService.MENTION_URL.format(entity_type, entity, limit)
+        url_mentions = JensenLabService.MENTION_URL.format(
+            entity_type, entity, limit)
         results = requests.get(url_mentions)
         assert results.ok
         publications_string = results.content.decode().strip()
@@ -65,5 +68,6 @@ class JensenLabService(TextMiningService):
 if __name__ == '__main__':
     text_mining_service = JensenLabService()
     print("Using service {}".format(text_mining_service.name))
-    publications = text_mining_service.get_mentions(["DOID:10652", "DOID:10935"], limit=1000000)
+    publications = text_mining_service.get_mentions(
+        ["DOID:10652", "DOID:10935"], limit=1000000)
     print(", ".join([p.pm_id for p in publications]))
