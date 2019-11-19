c0e8fdb3985e9812e83f89e8c26203d520707626
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 11:34:15 +0100

    moved publication to models. import classes from models

--- projects/11/interface/TextMiningService.py
+++ projects/11/interface/TextMiningService.py
@@ -1,15 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from typing import List
-
-
-class Publication:
-    def __init__(self, pmc_id=None, pm_id=None, doi=None, preprint_id=None, other_id=None):
-        self.pmc_id = pmc_id
-        self.pm_id = pm_id
-        self.doi = doi
-        self.preprint_id = preprint_id
-        self.other_id = other_id
-
+from models.publication import Publication
+from models.coocurrence import CoOccurrence
 
 class TextMiningService(metaclass=ABCMeta):
 
