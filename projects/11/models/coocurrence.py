78bacce07ab5252806e0aa5ba1f6f05b1c37474d
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 14:45:12 +0100

    added cooccurrence representation

--- projects/11/models/coocurrence.py
+++ projects/11/models/coocurrence.py
@@ -2,3 +2,6 @@ class CoOccurrence:
     def __init__(self, entity: str, score: float):
         self.entity = entity
         self.score = score
+
+    def __repr__(self):
+        return f'{self.entity} ({self.score})'
