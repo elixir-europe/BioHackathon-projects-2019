c4a09934c6acb55c2a8723098a3ead8ccdba89b8
Carlos Vega <carlos.vega@uni.lu>
2019-11-19 09:57:41 +0100

    first draft

---
+++ projects/11/interface/TextMiningService.py
@@ -0,0 +1,30 @@
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+class Publication():
+    def __init__(self, pmc_id, pm_id, doi, preprint_id, other_id):
+        self.pmc_id = pmc_id
+        self.pm_id = pm_id
+        self.doi = doi
+        self.preprint_id = preprint_id
+        self.other_id = other_id
+
+
+class TextMiningService(metaclass=ABCMeta):
+    
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+    
+    @abstractmethod
+    def getMentions(entities: List) -> List[Publication]:
+        pass
+
+    @abstractmethod
+    def getCoOccurrences(entity: str) -> List[str]:
+        pass
+
+
+    
+
+
