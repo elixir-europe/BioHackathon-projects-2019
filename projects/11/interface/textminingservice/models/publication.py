b76821c4338b004b2ce4ecd39b130052d0ff645a
Carlos Vega <carlos.vega@uni.lu>
2019-11-20 10:48:23 +0100

    added fields to publication class

--- projects/11/interface/textminingservice/models/publication.py
+++ projects/11/interface/textminingservice/models/publication.py
@@ -1,7 +1,10 @@
 class Publication:
-    def __init__(self, pmc_id=None, pm_id=None, doi=None, preprint_id=None, other_id=None):
+    def __init__(self, title=None, year=None, journal_title=None, pmc_id=None, pm_id=None, doi=None, preprint_id=None, other_id=None):
+        self.title = title
+        self.year = year
+        self.journal_title = journal_title
         self.pmc_id = pmc_id
-        self.pm_id = str(pm_id)
+        self.pm_id = str(pm_id)  # pubmed_id
         self.doi = doi
         self.preprint_id = preprint_id
         self.other_id = other_id
