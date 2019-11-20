a715f8045d4b53a2d40610035b1f42c3cf41dc51
6br <tfdvrt@gmail.com>
2019-11-21 06:11:44 +0900

    Update queries

--- projects/29/src/web_server/neo4j_wrapper.py
+++ projects/29/src/web_server/neo4j_wrapper.py
@@ -9,6 +9,9 @@ with open('config.yaml') as fd:
 
 driver = GraphDatabase.driver(CONFIG['neo4j']['neo4j_endpoint'], auth=(CONFIG['neo4j']['user'], CONFIG['neo4j']['pass']))
 
+def print_node_types(tx):
+    return tx.run("MATCH (n) OPTIONAL MATCH (n)-[r]-() RETURN distinct keys(n) AS r")
+
 
 def print_publications(tx, doi):
     #return tx.run("MATCH (n1 {id: 'http://identifiers.org/doi/10.1101/476457'})-[r]->(n2) RETURN r, n1, n2 LIMIT 25")
