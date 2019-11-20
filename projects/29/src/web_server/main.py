cd2cc0f6e09664a142c98ee84f97ef4a096ebdfe
6br <tfdvrt@gmail.com>
2019-11-21 07:31:09 +0900

    Change a sort method for retrieving publications

--- projects/29/src/web_server/main.py
+++ projects/29/src/web_server/main.py
@@ -1,12 +1,8 @@
 import os
 
 from fastapi import FastAPI
-# from fastapi_contrib.pagination import Pagination
-
 from pydantic import BaseModel
-
-from web_server.sparql_wrapper import execute_query, get_total_papers
-from web_server.neo4j_wrapper import execute_cypher, test, get_properties, execute_cypher_simple
+from web_server.neo4j_wrapper import execute_cypher, test, get_properties, execute_cypher_simple, execute_cypher_update
 
 class Query(BaseModel):
     q: list = []
@@ -31,16 +27,11 @@ def query(q: Query):
     res = execute_cypher(q.q)
     return res
 
-#@app.get('/api/v1/query')
-#def query(q: str = None, page: int = 1):
-#    res = execute_query({"q": q})
-    #pagination = Pagination(
-    #page=page, page_parameter=page, per_page_parameter=10, found=len(res), total=get_total_papers(),
-    #record_name='papers', format_total=True, format_number=True,
-    #search=True, bs_version=4)
-    #res = list(res)[(int(page) * 10 - 10):(int(page) * 10)]
-#    return res 
 
+@app.post('/api/v1/update')
+def update(doi: str = None, score: int):
+    res = execute_cypher_update(doi, score)
+    return res
 
 
 #if __name__ == '__main__':
