997ee7609f1c13e52bbd9ee90dc025a4898598cb
6br <tfdvrt@gmail.com>
2019-11-21 05:39:31 +0900

    Add converter for neo4j where clause

--- projects/29/src/web_server/neo4j_wrapper.py
+++ projects/29/src/web_server/neo4j_wrapper.py
@@ -17,9 +17,54 @@ def print_publications(tx, doi):
     #return tx.run("MATCH (n1 {id: '$doi'})-[r]->(n2) RETURN r, n1, n2 LIMIT 25", doi=doi)
 
 
-def execute_cypher(query):
+def print_publications(tx, where):
+    #return tx.run("MATCH (n1 {id: 'http://identifiers.org/doi/10.1101/476457'})-[r]->(n2) RETURN r, n1, n2 LIMIT 25")
+    #print("MATCH (n1 {id: " + doi + "})-[r]->(n2) RETURN r, n1, n2 LIMIT 25")
+    print("MATCH (n1)-[r]->(n2) WHERE " + where + " RETURN r, n1, n2 LIMIT 25")
+    return tx.run("MATCH (n1)-[r]->(n2) WHERE " + where + " RETURN r, n1, n2 LIMIT 25")
+    #return tx.run("MATCH (n1 {id: '$doi'})-[r]->(n2) RETURN r, n1, n2 LIMIT 25", doi=doi)
+
+
+def execute_cypher_simple(query):
     with driver.session() as session:
         value = session.read_transaction(print_publications, query)
+        #print(list(value.records()))
+        #print([record["r"] for record in value.records()])
+        return {"values": [(record["r"], record["n2"]) for record in value.records()]}
+
+def test():
+    print(category_builder([{"category":"topic","operator":"==","value":"Query and retrieval"}]))
+    print(category_builder([{"expressions":[{"category":"topic","operator":"==","value":"Query and retrieval"}]},{"expressions":[{"expressions":[{"category":"author","operator":"contains","value":"Steffen"}]},{"expressions":[{"category":"year","operator":"!=","value":"2019"}],"conditionType":"AND"}],"conditionType":"AND"}]))
+
+def category_builder(query_array):
+    TMP = ""
+    for item in query_array:
+        if "value" in item:
+            value = item["value"] if item["value"].isnumeric() else '"' + item["value"] + '"'
+        if "conditionType" in item:
+            TMP += " " + item["conditionType"] + " "
+        if "operator" in item:
+            if item["operator"] == "==":
+                operator = "="
+                TMP += 'n1.' + item["category"] + ' ' + operator + " " + value
+            elif item["operator"] == "!=":
+                operator = "!="
+                TMP += 'n1.' + item["category"] + ' ' + operator + " " + value
+            elif item["operator"] == "contains":
+                operator = "CONTAINS"
+                TMP += 'n1.' + item["category"] + ' ' + operator + " " + value
+            elif item["operator"] == "!contains":
+                operator = "CONTAINS"
+                TMP += "NOT (" + 'n1.' + item["category"] + ' ' + operator + " " + value + ")"
+        
+        if "expressions" in item:
+            TMP += "(" + category_builder(item["expressions"]) + ")"
+    return TMP
+
+
+def execute_cypher(query_array):
+    with driver.session() as session:
+        value = session.read_transaction(print_publications, category_builder(query_array))
         #print(list(value.records()))
         #print([record["r"] for record in value.records()])
         return {"values": [(record["r"], record["n2"]) for record in value.records()]}
