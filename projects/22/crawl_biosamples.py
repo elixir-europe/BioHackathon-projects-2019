4ba32e0c15e2842b198c7d4316a4d149e23a65b1
Isuru Liyanage <theisuru@gmail.com>
2019-11-20 11:38:49 +0000

    add validation, optimize code

--- projects/22/crawl_biosamples.py
+++ projects/22/crawl_biosamples.py
@@ -1,28 +1,49 @@
 import json
 
 import requests
+from decouple import config
 
-import config
+import neo_utils
 
 request_timeout = 50
 
 
+biosamples_url = config('biosamples_url')
+output_file = config('data_file')
+
+
+class BioSamplesCrawler:
+    def __init__(self, page_size=1000, project=""):
+        self.project = project
+        self.page_size = page_size
+
+        self.page_url = biosamples_url + "?cursor=*&size=" + str(page_size)
+        if project != "":
+            self.page_url += "&filter=attr:project:" + project
+
+    def get_next_page(self):
+        samples, self.page_url = get_samples(self.page_url)
+        return samples
+
+
 def main():
-    # save_samples(project="FAANG")
-    save_samples()
+    # save_samples_to_one_big_file(project="FAANG")
+    # save_samples_to_one_big_file()
+    # load_and_save_samples_to_files(project="FAANG")
+    load_and_save_samples_to_files()
 
 
 # Why we are not using multi-threading?
 # BioSample uses Solr cursor to retrieve next set of samples in a page.
 # If we use page, skip instead of using this cursor, Biosamples can time-out
-def save_samples(limit=0, project=""):
+def save_samples_to_one_big_file(limit=0, project=""):
     sample_count = 0
 
-    with open(config.output_file, "w") as output:
+    with open(output_file, "w") as output:
         output.write("[\n")
 
-    with open(config.output_file, "a") as output:
-        url = config.biosamples_url + "?cursor=*&size=1000"
+    with open(output_file, "a") as output:
+        url = biosamples_url + "?cursor=*&size=1000"
         if project != "":
             url = url + "&filter=attr:project:" + project
 
@@ -48,6 +69,53 @@ def save_samples(limit=0, project=""):
         print("Total samples collected: " + str(sample_count))
 
 
+def load_and_save_samples_to_files(project=""):
+    sample_count = 0
+    file_no = 1
+    max_sample_per_file = 1000000
+
+    url = biosamples_url + "?cursor=*&size=1000"
+    if project != "":
+        url = url + "&filter=attr:project:" + project
+
+    sample_list = []
+    while url != "":
+        samples, url = get_samples(url)
+        sample_list += samples
+        sample_count += len(samples)
+
+        if len(sample_list) >= max_sample_per_file:
+            output_file_name = "data/samples_" + project + "_" + str(file_no) + ".json"
+            print("Sample count: " + str(sample_count) + ", flushing " + str(len(sample_list)) + " samples to: " + output_file_name)
+            with open(output_file_name, "w") as data_file:
+                data_file.write(json.dumps(sample_list, indent=4))
+
+            file_no += 1
+            sample_list = []
+
+    output_file_name = "data/samples_" + project + "_" + str(file_no) + ".json"
+    print("Sample count: " + str(sample_count) + ", flushing " + str(len(sample_list)) + " samples to: " + output_file_name)
+    with open(output_file_name, "w") as data_file:
+        data_file.write(json.dumps(sample_list, indent=4))
+
+
+def load_and_save_samples_to_db(project=""):
+    sample_count = 0
+
+    url = biosamples_url + "?cursor=*&size=1000"
+    if project != "":
+        url = url + "&filter=attr:project:" + project
+
+    while url != "":
+        samples, url = get_samples(url)
+        sample_count += len(samples)
+
+        if sample_count % 100000:
+            print("Got " + str(sample_count) + " samples")
+
+        neo_utils.save_samples(samples)
+
+
 def get_samples(url):
     response = requests.get(url, timeout=request_timeout)
     samples = []
@@ -63,6 +131,20 @@ def get_samples(url):
     else:
         print("invalid response code from biosamples")
         response.raise_for_status()
+
+    return samples, url
+
+
+def get_samples_with_retry(url):
+    samples = []
+    try:
+        samples, url = get_samples(url)
+    except requests.exceptions.ReadTimeout:
+        # todo add retry after some time
+        print("Read timeout.")
+    except requests.exceptions.HTTPError:
+        # todo add retry after some time
+        print("Internal server error. Server might be too busy")
 
     return samples, url
 
--- projects/22/neo_utils.py
+++ projects/22/neo_utils.py
@@ -1,6 +1,5 @@
+from decouple import config
 from py2neo import Graph
-
-import config
 
 
 def execute_query(query):
@@ -9,11 +8,18 @@ def execute_query(query):
     return response
 
 
+def save_samples(samples):
+    graph = connect_to_graph()
+    for sample in samples:
+        print("saving to graph")
+        # todo save to graph
+
+
 def clear_graph():
     graph = connect_to_graph()
     graph.delete_all()
 
 
 def connect_to_graph():
-    graph = Graph(config.neo4j_url, user=config.neo4j_user, password=config.neo4j_password)
+    graph = Graph(config('neo4j_url'), user=config('neo4j_user'), password=config('neo4j_password'))
     return graph
