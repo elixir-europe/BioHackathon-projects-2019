4ec8ecffc969386f2ef65e07bc48567b1728f3d5
Margarita Kopniczky <maboko@maboko-macbookpro.roam.corp.google.com>
2019-11-21 11:36:21 +0000

    get mentions from server

--- projects/11/web/scripts.js
+++ projects/11/web/scripts.js
@@ -1,15 +1,34 @@
 var app = new Vue({
   el: "#container",
   data: {
-    todos: []
+    entity: "",
+    papers: []
   },
   methods: {
+    //refactor this for a hard coded example for demo
     getPapers: function() {
-      this.$set(this, "todos", [
-        { text: "Learn JavaScript" },
-        { text: "Learn Vue" },
-        { text: "Build something awesome" }
+      this.$set(this, "papers", [
+        { text: "Paper 1 Title" },
+        { text: "Some Other Paper" },
+        { text: "Third Paper Title" }
       ]);
+    },
+    getMentions: function() {
+      var that = this;
+      var entity = that.entity;
+      var url = `http://9b9a4973.ngrok.io/getMentions/?entity=${entity}?limit=100`;
+      console.log(url);
+      var jqxhr = $.ajax(url)
+        .done(function(data) {
+          data = JSON.parse(data);
+
+          that.$set(that, "papers", data);
+          console.log("success", data);
+        })
+        .fail(function() {
+          alert("error");
+        })
+        .always(function() {});
     }
   }
 });
