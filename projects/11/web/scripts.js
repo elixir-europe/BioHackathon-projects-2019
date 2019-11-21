82177c91ae805744d8f5074d72e2c7c8b97a6b6a
Carlos Vega <carlos.vega@uni.lu>
2019-11-21 19:16:25 +0100

    added DOI lookup for those papers without it

--- projects/11/web/scripts.js
+++ projects/11/web/scripts.js
@@ -36,6 +36,7 @@ var app = new Vue({
             that.$set(that, "entities", []);
             that.$set(that, "papers", data);
             console.log("success mentions", data);
+            that.checkDOIs();
           })
           .fail(function () {
             alert("error");
@@ -47,7 +48,6 @@ var app = new Vue({
       var that = this;
       var entity = that.entity;
       var url = "http://9b9a4973.ngrok.io/getCooccurrence/DOID:2841?type=9606";
-      console.log(url);
       if (!entity) {
         alert("Enter an entity!");
       } else {
@@ -62,9 +62,43 @@ var app = new Vue({
           })
           .fail(function () {
             alert("error");
-          })
-          .always(function () { });
+          });
       }
+    },
+    checkDOIs: function () {
+      var that = this;
+      _.forEach(app.papers, function (paper, idx) {
+        if (paper.info.doi == undefined) {
+          if (paper.info.pm_id != undefined) {
+            that.getDOI(paper.info.pm_id, 'pm_id', idx, that);
+          } else if (paper.info.pmc_id != undefined) {
+            that.getDOI(paper.info.pm_id, 'pmc_id', idx, that);
+          }
+        }
+      });
+    },
+    getDOI: function (id, type, pub_index, vue_app) {
+      if (type == 'pmc_id') {
+        var idtype = "pmcid";
+      } else if (type == 'pm_id') {
+        var idtype = "pmid";
+      } else {
+        var idtype = "";
+      }
+
+      var url = `https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids=${id}&format=json`;
+
+      var jqxhr = $.ajax(url)
+        .done(function (data) {
+          if (data.records.length > 0) {
+            if (data.records[0].doi != undefined) {
+              vue_app.$set(vue_app.papers[pub_index].info, 'doi', data.records[0].doi);
+            }
+          }
+        }).fail(function () {
+          console.log(`Error for ${id} ${type} ${pub_index} and url ${url}`);
+        });
+
     }
   }
 });
