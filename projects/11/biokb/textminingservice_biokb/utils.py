5dc777ef07521be0d323f65edb0ba37babb7b067
Carlos Vega <carlos.vega@uni.lu>
2019-11-21 16:39:01 +0100

    added functions to work with entity types

--- projects/11/biokb/textminingservice_biokb/utils.py
+++ projects/11/biokb/textminingservice_biokb/utils.py
@@ -7,6 +7,12 @@ def standardise_underscored_entity_code(entity_code: str) -> str:
     return entity_code.replace(':', '_', 1)
 
 
+def standardise_entity_type(entity_type: str) -> str:
+    if entity_type is not None:
+        return entity_type.split('#')[-1]
+    return entity_type
+
+
 def uri_to_entity_code(uri: str) -> str:
     """Translates URIs such as http://lcsb.uni.lu/biokb/entities/BTO_0001043 to BTO:0001043
     Replaces only the first underscore with colon.
@@ -36,5 +42,6 @@ def reflect_type_to_biokb(entity_type: int) -> str:
     if entity_type > 0:
         return 'http://lcsb.uni.lu/biokb#Protein'
     translated = reflect_types.get(entity_type, None)
-    logger.warning(f'Type {entity_type} not found for BioKB.')
+    if translated is None:
+        logger.warning(f'Type {entity_type} not found for BioKB.')
     return translated
